e7663fd5cd0f84f88ab1abc0554c961bac650b06
Slutishboy <83270202+Slutishboy@users.noreply.github.com>
2022-08-09 03:02:01 +0530

    day3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
-import 'package:fapp/home_page.dart';
+import 'package:fapp/pages/home_page.dart';
+import 'package:fapp/pages/login_page.dart';
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
-      );
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.brown),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/login": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+      },
+    );
   }
 }
