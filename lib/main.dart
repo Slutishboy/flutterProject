86a48c21994d8733e4d6eb23d64f2949752a199e
Slutishboy <83270202+Slutishboy@users.noreply.github.com>
2022-08-09 01:02:49 +0530

    Day2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,15 @@
+import 'package:fapp/home_page.dart';
 import 'package:flutter/material.dart';
-
 void main() {
   runApp(MyApp());
 }
 
-class  MyApp extends StatelessWidget {
-  
+class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("helo world"),
-          ),
-        ),
-      ),
-    );
+      home: HomePage(),
+      );
   }
 }
